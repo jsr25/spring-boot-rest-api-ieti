5e68febd5bd038975643118203630249886ca7d2
jsr25 <ramosjuans@live.com.ar>
2021-08-19 17:42:21 -0500

    creation user in data

---
+++ src/main/java/co/eci/edu/ieti/apirest/data/User.java
@@ -0,0 +1,13 @@
+package co.eci.edu.ieti.apirest.data;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String lastName;
+
+    private Boolean created;
+}
