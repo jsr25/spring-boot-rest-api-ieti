4609d4cdd2c1050b0c129106eaf2bc9742a19ac4
jsr25 <ramosjuans@live.com.ar>
2021-08-19 17:48:06 -0500

    basic implementation service

---
+++ src/main/java/co/eci/edu/ieti/apirest/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package co.eci.edu.ieti.apirest.service.impl;
+
+import co.eci.edu.ieti.apirest.data.User;
+import co.eci.edu.ieti.apirest.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public User findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<User> all() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(String id) {
+
+    }
+
+    @Override
+    public User update(User user, String userId) {
+        return null;
+    }
+}
